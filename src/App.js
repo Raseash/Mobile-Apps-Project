c2211d53be25a4def2e8cda47508e08a55ae7f2f
unknown <alex@example.com>
2024-05-01 20:48:46 -0500

    Started

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+
+
 
 function App() {
   return (
-    <div className="App">
-      
+    <div  className="App">
+       <div className = "leftDiv">
+          This is the left div
+      </div>
+      <div className = "rightDiv">
+        This is the right div
+      </div>
       <StockList />
     </div>
   );
