cd90c252d441398c5ab773bfa4ea33df2353f5f7
unknown <alex@example.com>
2024-05-06 19:29:08 -0500

    updated app to use used states

--- src/App.js
+++ src/App.js
@@ -1,42 +1,63 @@
 import './App.css';
+import appleLogo from './AppleLogo.JPG';
+import { useState } from 'react';
 
+export default function MyApp() {
+  const [count, setCount] = useState(0);
+
+  function handleClick() {
+    setCount(count + 1);
+  }
+
+  return (
+    <div>
+      <h1>Counters that update together</h1>
+      <MyButton count={count} onClick={handleClick} />
+      <MyButton count={count} onClick={handleClick} />
+    </div>
+  );
 
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  function handleClick() {
+    setCount(count + 1);
+  }
   return (
     <div  className="App">
        <div className = "leftDiv">
           This is the left div
+          <AppleStock />
+          <StockList count={count} onClick={handleClick}  />
       </div>
       <div className = "rightDiv">
         This is the right div
+        {count}
       </div>
-      <StockList />
-    </div>
-  );
-}
-function BuyButton() {
-  return (
-    <button>
-       buy
-    </button>
-  );
-}
-function SellButton() {
-  return (
-    <button>
-       sell
-    </button>
-  );
-}
- function StockList() {
-  return (
-    <div>
-      <h1>Welcome to my app</h1>
-      <SellButton />
-      <BuyButton />
     </div>
   );
 }
 
+
+function StockList({count, onClick} ) {
+  return (
+    <div>
+      <h3>Buy and sell your shares</h3>
+      <button onClick={onClick}>
+      bought {count} times
+      </button>
+      <button>Sell</button>
+    </div>
+  );
+}
+
+function AppleStock() {
+  return (
+    <div>
+      <img src={appleLogo} width = "100" />
+      <p>Current: $163</p>
+    </div>
+  );
+}
 export default App;
