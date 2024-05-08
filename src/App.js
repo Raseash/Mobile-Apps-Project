a5550c17a22c1d8e307452e4e957b402ecd89e04
unknown <alex@example.com>
2024-05-07 19:22:08 -0500

    added buy and sell buttons

--- src/App.js
+++ src/App.js
@@ -2,52 +2,50 @@ import './App.css';
 import appleLogo from './AppleLogo.JPG';
 import { useState } from 'react';
 
-export default function MyApp() {
-  const [count, setCount] = useState(0);
-
-  function handleClick() {
-    setCount(count + 1);
-  }
-
-  return (
-    <div>
-      <h1>Counters that update together</h1>
-      <MyButton count={count} onClick={handleClick} />
-      <MyButton count={count} onClick={handleClick} />
-    </div>
-  );
-
 
 function App() {
   const [count, setCount] = useState(0);
 
-  function handleClick() {
+  function addCount() {
     setCount(count + 1);
+  }
+  function subtractCount() {
+    setCount(count - 1);
   }
   return (
     <div  className="App">
        <div className = "leftDiv">
           This is the left div
           <AppleStock />
-          <StockList count={count} onClick={handleClick}  />
+          <StockList count={count} onAdd={addCount} onSubtract={subtractCount} />
       </div>
       <div className = "rightDiv">
-        This is the right div
-        {count}
+        <p>Number of Shares bought {count}</p>
       </div>
     </div>
   );
 }
 
 
-function StockList({count, onClick} ) {
+function StockList({count, onAdd, onSubtract} ) {
   return (
     <div>
       <h3>Buy and sell your shares</h3>
-      <button onClick={onClick}>
-      bought {count} times
+      <TextField
+          id="filled-number"
+          label="Number"
+          type="number"
+          InputLabelProps={{
+            shrink: true,
+          }}
+          variant="filled"
+        />
+      <button onClick={onAdd}>
+      Buy one share
       </button>
-      <button>Sell</button>
+      <button onClick={onSubtract}>
+      Sell one share
+      </button>
     </div>
   );
 }
@@ -56,8 +54,9 @@ function AppleStock() {
   return (
     <div>
       <img src={appleLogo} width = "100" />
-      <p>Current: $163</p>
+      <p>Current Price: $163</p>
     </div>
   );
 }
+
 export default App;
